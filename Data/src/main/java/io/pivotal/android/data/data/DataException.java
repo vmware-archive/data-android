7be647fd7ac463748d94ee86fa04feef28f38a11
Joshua Winters <joshua@xtremelabs.com>
2014-07-08 14:50:21 -0400

    @pivotal-jwinters - made DataException a RuntimeException

--- Data/src/main/java/io/pivotal/android/data/data/DataException.java
+++ Data/src/main/java/io/pivotal/android/data/data/DataException.java
@@ -1,6 +1,6 @@
 package io.pivotal.android.data.data;
 
-public class DataException extends Exception {
+public class DataException extends RuntimeException {
 
     public DataException(String detailMessage) {
         super(detailMessage);
