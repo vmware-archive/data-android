2b16d7b0f7e891599ce027b5d40314de158da268
Rob Szumlakowski <robsz@pivotallabs.com>
2014-10-24 15:33:45 -0400

    @pivotal-rszumlakowski - changed logout API to hit the new logout endpoint on the mobile identity server. set version to 1.0.4. [#81396182]

--- data/src/main/java/io/pivotal/android/data/client/LogoutEngine.java
+++ data/src/main/java/io/pivotal/android/data/client/LogoutEngine.java
@@ -4,20 +4,13 @@
 package io.pivotal.android.data.client;
 
 import com.google.api.client.auth.oauth2.Credential;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.lang.reflect.Type;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.List;
 
 import io.pivotal.android.data.api.ApiProvider;
 import io.pivotal.android.data.api.AuthorizedApiRequest;
-import io.pivotal.android.data.model.AccessTokenInfo;
 import io.pivotal.android.data.prefs.AuthorizationPreferencesProvider;
 import io.pivotal.android.data.util.Logger;
 
@@ -50,14 +43,14 @@ public class LogoutEngine extends AbstractAuthorizationClient {
                     // Already logged out. Considered successful.
                     return;
                 }
-                getTokenId(credential);
+                logoutFromAuthProvider(credential);
             }
         });
     }
 
-    private void getTokenId(final Credential credential) throws AuthorizationException {
+    private void logoutFromAuthProvider(final Credential credential) throws AuthorizationException {
         final AuthorizedApiRequest request = getRequest();
-        final URL getTokenIdUrl = getTokenIdUrl();
+        final URL logoutUrl = getLogoutUrl();
         final AuthorizedApiRequest.HttpOperationListener listener = new AuthorizedApiRequest.HttpOperationListener() {
 
             @Override
@@ -67,26 +60,8 @@ public class LogoutEngine extends AbstractAuthorizationClient {
                     return;
                 }
 
-                if (contentType == null || !contentType.startsWith("application/json")) {
-                    onFailure("Server returned bad content type: " + contentType);
-                    return;
-                }
-
-                if (result == null) {
-                    onFailure("Server returned empty data.");
-                    return;
-                }
-
-                final List<AccessTokenInfo> accessTokens = parseAccessTokenInfoList(result);
-                final AccessTokenInfo accessToken = AccessTokenInfo.findItemMatchingCredential(credential, accessTokens);
-
-                if (accessToken == null) {
-                    Logger.v("Server does not have a token that matches our saved credential. Nothing to delete from server.");
-                    deleteCredential();
-                    return;
-                }
-
-                deleteToken(credential, accessToken.id);
+                Logger.v("Successfully logged out from authorization server.");
+                deleteCredential();
             }
 
             @Override
@@ -100,37 +75,7 @@ public class LogoutEngine extends AbstractAuthorizationClient {
             }
         };
 
-        request.executeHttpRequest("GET", getTokenIdUrl, null, null, "UTF-8", null, credential, authorizationPreferencesProvider, listener);
-    }
-
-    private void deleteToken(final Credential credential, final String id) {
-        final AuthorizedApiRequest request = getRequest();
-        final URL deleteTokenUrl = getDeleteTokenUrl(id);
-        final AuthorizedApiRequest.HttpOperationListener listener = new AuthorizedApiRequest.HttpOperationListener() {
-
-            @Override
-            public void onSuccess(int httpStatusCode, String contentType, String contentEncoding, InputStream result) {
-                if (!isSuccessHttpStatusCode(httpStatusCode)) {
-                    onFailure("Server returned HTTP status code: " + httpStatusCode);
-                    return;
-                }
-
-                Logger.v("Successfully deleted access token from server.");
-                deleteCredential();
-            }
-
-            @Override
-            public void onUnauthorized() {
-                Logger.e("Unauthorized result trying to delete access token from identity server.");
-            }
-
-            @Override
-            public void onFailure(String reason) {
-                Logger.e("Error deleting access token from identity server: " + reason);
-            }
-        };
-
-        request.executeHttpRequest("DELETE", deleteTokenUrl, null, null, "UTF-8", null, credential, authorizationPreferencesProvider, listener);
+        request.executeHttpRequest("GET", logoutUrl, null, null, "UTF-8", null, credential, authorizationPreferencesProvider, listener);
     }
 
     private void deleteCredential() {
@@ -149,30 +94,12 @@ public class LogoutEngine extends AbstractAuthorizationClient {
         return request;
     }
 
-    private List<AccessTokenInfo> parseAccessTokenInfoList(final InputStream stream) {
-        final Gson gson = new Gson();
-        final Type listType = new TypeToken<List<AccessTokenInfo>>() {}.getType();
-        final Reader reader = new InputStreamReader(stream);
-        final List<AccessTokenInfo> list = gson.fromJson(reader, listType);
-        return list;
-    }
-
     private boolean isSuccessHttpStatusCode(final int httpStatusCode) {
         return httpStatusCode >= 200 && httpStatusCode < 300;
     }
 
-    private URL getTokenIdUrl() throws AuthorizationException {
-        final String base = authorizationPreferencesProvider.getAuthorizationUrl() + "/api/tokens/access";
-        try {
-            final URL url = new URL(base);
-            return url;
-        } catch (MalformedURLException e) {
-            throw new AuthorizationException(e.getMessage());
-        }
-    }
-
-    private URL getDeleteTokenUrl(String id) throws AuthorizationException {
-        final String base = authorizationPreferencesProvider.getAuthorizationUrl() + "/api/tokens/access/" + id;
+    private URL getLogoutUrl() throws AuthorizationException {
+        final String base = authorizationPreferencesProvider.getAuthorizationUrl() + "/oauth/logout";
         try {
             final URL url = new URL(base);
             return url;
