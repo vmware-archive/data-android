e160543e7a9fd75bd041d9331823cff61755fc3c
Devin Fallak and Joshua Winters <dfallak@pivotal.io>
2015-01-26 15:01:04 -0500

    Added more android os build data to user agent header

--- data/src/androidTest/java/io/pivotal/android/data/RemoteClientTest.java
+++ data/src/androidTest/java/io/pivotal/android/data/RemoteClientTest.java
@@ -185,7 +185,7 @@ public class RemoteClientTest extends AndroidTestCase {
         client.addUserAgentHeader(httpRequest);
 
         final String sdkVersion = String.format("PCFData/%s", BuildConfig.SDK_VERSION);
-        final String androidVersion = String.format("(Android; %d; %s)", Build.VERSION.SDK_INT, Build.VERSION.RELEASE);
+        final String androidVersion = String.format("[Android Version %s (Build %s)]", Build.VERSION.RELEASE, Build.ID);
         Mockito.verify(httpRequest).addHeader(RemoteClient.Default.Headers.USER_AGENT, sdkVersion + " " + androidVersion);
     }
 
--- data/src/main/java/io/pivotal/android/data/RemoteClient.java
+++ data/src/main/java/io/pivotal/android/data/RemoteClient.java
@@ -176,7 +176,7 @@ public interface RemoteClient<T> {
 
         protected void addUserAgentHeader(final HttpUriRequest request) {
             final String sdkVersion = String.format("PCFData/%s", BuildConfig.SDK_VERSION);
-            final String androidVersion = String.format("(Android; %d; %s)", Build.VERSION.SDK_INT, Build.VERSION.RELEASE);
+            final String androidVersion = String.format("[Android Version %s (Build %s)]", Build.VERSION.RELEASE, Build.ID);
             request.addHeader(Headers.USER_AGENT, sdkVersion + " " + androidVersion);
         }
 
